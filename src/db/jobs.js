153a231c98033863563e47f1704190edc7098ac0
James <james.t@getreworked.com>
2022-05-18 13:40:55 +1000

    refactor: Updates Experience

--- src/db/jobs.js
+++ src/db/jobs.js
@@ -1,112 +1,104 @@
 export const jobs = [
-    {
-      "title": "Senior Frontend Engineer",
-      "company": "Where (Cauzey Ltd.)",
-      "years": ["2022", "2021", "2020"],
-      "list": [
-        "Lead frontend app architecture design & development",
-        "Shifted our tech stack to Typescript, React, React-Query, Vite, Tailwind",
-        "Integration with Google geolocation, places, & map services",
-        "Integration with Stripe payment processing",
-        "Helped guide UI/UX in developing our design system using Tailwind"
-      ]
-    },
-    {
-      "title": "Senior Frontend Engineer",
-      "company": "Complish (Cauzey Ltd.)",
-      "years": ["2022", "2021", "2020"],
-      "list": [
-        "Joined to help build out new flagship features for Complish's Vue web app",
-        "Lead frontend implementation of Discussions - a feature involving a highly customised collaborative document editor powered by ProseMirror",
-        "Championed shift to state management using ORM",
-        "Integrated real-time collaboration & notification features using Pusher",
-        "Refactored existing features to improve DX & improve app performance"
-      ]
-    },
-    {
-      "title": "Senior Frontend Developer",
-      "company": "Communo",
-      "years": ["2020", "2019"],
-      "list": [
-        "Contributed to the development of their web SPA & native mobile app",
-        "Owned the development of chat feature set and integration with Twilio chat API",
-        "Worked closely with UI/UX designer and project lead to meet milestones",
-        "Contributed to app architecture, performance, and debugging"
-      ]
-    },
-    {
-      "title": "Senior Frontend Developer",
-      "company": "BRND WGN",
-      "years": ["2019", "2018", "2017", "2016"],
-      "list": [
-        "Project lead for development of Casino.eu ecosystem which includes customer and admin web applications",
-        "Worked closely with product managers to meet client expectations",
-        "Advised UX/UI designers, ensuring concepts were achievable and calculated",
-        "Collaborated with backend team speccing and planning feature requests",
-        "Independently handled development of micro projects"
-      ]
-    },
-    {
-      "title": "Lead UX/UI Web Developer",
-      "company": "nTrust Inc.",
-      "years": ["2015", "2014", "2013"],
-      "list": [
-        "Lead front end development of multiple web applications",
-        "Guided development of web applications to achieve responsive resolutions across mobile & desktop devices",
-        "Modularized & optimized web application infrastructure",
-        "Worked closely with UX/UI designers to ensure concepts could be efficiently implemented",
-        "Worked closely with product team to conceptualize new features",
-        "Liaisoned with backend team to ensure smooth development cycles"
-      ]
-    },
-    {
-      "title": "Lead Web UI Designer / Developer",
-      "company": "TBD Digital",
-      "years": ["2012", "2011", "2010", "2009"],
-      "list": [
-        "Interface design, graphic design, storyboard design",
-        "Worked with project managers to meet client requests",
-        "Front-end web development",
-        "Mentoring junior web developers",
-        "Mobile app & web design",
-        "HTML email design & development",
-        "Information architecture, wireframes, sitemaps"
-      ]
-    },
-    {
-      "title": "Freelance Web Designer / Developer",
-      "company": "Self Employed",
-      "years": ["2009", "2008"],
-      "list": [
-        "Interface & graphic design",
-        "Front-end web development",
-        "HTML email design & coding",
-        "Information architecture, wireframes, sitemaps",
-        "Project management, budgets, time frames",
-        "SEO (Keywords, Analytics, AdWords)"
-      ]
-    },
-    {
-      "title": "Apple Technician",
-      "company": "Mac Station",
-      "years": ["2008", "2007"],
-      "list": [
-        "Hardware & software repairs",
-        "Diagnostic & repair",
-        "Onsite diagnostics & repair",
-        "Server setup & configuration"
-      ]
-    },
-    {
-      "title": "Jr. Web Guru",
-      "company": "Cowie & Fox",
-      "years": ["2007", "2006"],
-      "list": [
-        "Interface design, graphic design",
-        "Front-end web development",
-        "Custom Wordpress themes",
-        "HTML email design & coding",
-        "Information architecture, wireframes, sitemaps"
-      ]
-    }
-  ]
+  {
+    title: "Senior Frontend Engineer",
+    company: "Cauzey Ltd. (Complish/Where)",
+    years: ["2022", "2021", "2020"],
+    list: [
+      "Joined to help build out new flagship features for Complish's Vue web app",
+      "Lead frontend implementation of Discussions - a feature involving a highly customised collaborative document editor powered by ProseMirror",
+      "Championed shift to state management using ORM",
+      "Integrated real-time collaboration & notification features using Pusher",
+      "Refactored existing features to improve DX & improve app performance",
+      "Lead frontend app architecture design & development for Where",
+      "Integrated 3rd party services (Google geolocation/places/map, Stripe payments, Intercom) into Where",
+      "Helped guide UI/UX in developing our design system for Where using Tailwind",
+      "Setup CI/CD pipeline for Where",
+    ],
+  },
+  {
+    title: "Senior Frontend Developer",
+    company: "Communo",
+    years: ["2020", "2019"],
+    list: [
+      "Contributed to the development of their web SPA & native mobile app",
+      "Owned the development of chat feature set and integration with Twilio chat API",
+      "Worked closely with UI/UX designer and project lead to meet milestones",
+      "Contributed to app architecture, performance, and debugging",
+    ],
+  },
+  {
+    title: "Senior Frontend Developer",
+    company: "BRND WGN",
+    years: ["2019", "2018", "2017", "2016"],
+    list: [
+      "Project lead for development of Casino.eu ecosystem which includes customer and admin web applications",
+      "Worked closely with product managers to meet client expectations",
+      "Advised UX/UI designers, ensuring concepts were achievable and calculated",
+      "Collaborated with backend team speccing and planning feature requests",
+      "Independently handled development of micro projects",
+    ],
+  },
+  {
+    title: "Lead UX/UI Web Developer",
+    company: "nTrust Inc.",
+    years: ["2015", "2014", "2013"],
+    list: [
+      "Lead front end development of multiple web applications",
+      "Guided development of web applications to achieve responsive resolutions across mobile & desktop devices",
+      "Modularized & optimized web application infrastructure",
+      "Worked closely with UX/UI designers to ensure concepts could be efficiently implemented",
+      "Worked closely with product team to conceptualize new features",
+      "Liaisoned with backend team to ensure smooth development cycles",
+    ],
+  },
+  {
+    title: "Lead Web UI Designer / Developer",
+    company: "TBD Digital",
+    years: ["2012", "2011", "2010", "2009"],
+    list: [
+      "Interface design, graphic design, storyboard design",
+      "Worked with project managers to meet client requests",
+      "Front-end web development",
+      "Mentoring junior web developers",
+      "Mobile app & web design",
+      "HTML email design & development",
+      "Information architecture, wireframes, sitemaps",
+    ],
+  },
+  {
+    title: "Freelance Web Designer / Developer",
+    company: "Self Employed",
+    years: ["2009", "2008"],
+    list: [
+      "Interface & graphic design",
+      "Front-end web development",
+      "HTML email design & coding",
+      "Information architecture, wireframes, sitemaps",
+      "Project management, budgets, time frames",
+      "SEO (Keywords, Analytics, AdWords)",
+    ],
+  },
+  {
+    title: "Apple Technician",
+    company: "Mac Station",
+    years: ["2008", "2007"],
+    list: [
+      "Hardware & software repairs",
+      "Diagnostic & repair",
+      "Onsite diagnostics & repair",
+      "Server setup & configuration",
+    ],
+  },
+  {
+    title: "Jr. Web Guru",
+    company: "Cowie & Fox",
+    years: ["2007", "2006"],
+    list: [
+      "Interface design, graphic design",
+      "Front-end web development",
+      "Custom Wordpress themes",
+      "HTML email design & coding",
+      "Information architecture, wireframes, sitemaps",
+    ],
+  },
+];
--- src/db/skillsets.js
+++ src/db/skillsets.js
@@ -1,85 +1,87 @@
 export const skillsets = [
-    {
-      "heading": "Knowledge",
-      "list": [
-        "JavaScript",
-        "Typescript",
-        "React",
-        "Vue",
-        "HTML",
-        "CSS",
-        "Git",
-        "Vite",
-        "Rollup",
-        "Webpack",
-        "GSAP",
-        "SVG",
-        "WebSockets",
-        "Express",
-        "AdonisJS",
-        "Ionic",
-        "Capacitor",
-        "Tailwind",
-        "Twilio APIs",
-        "Pusher",
-        "TipTap/ProseMirror"
-      ]
-    },
-    {
-      "heading": "",
-      "list": [
-        "SPA Architecture Design & Developement",
-        "CI/CD Methodology",
-        "Unit Testing",
-        "End-to-End Testing",
-        "Web Animation",
-        "Responsive Design & Development",
-        "Front End Web Architecture",
-        "Web Accessibility",
-        "Web Perfomance Optimization",
-        "Code Optimization",
-        "Gitflow Methodology",
-        "UI/UX Design",
-        "Wireframes, User Flows, Flowcharts"
-      ]
-    },
-    {
-      "heading": "Tools",
-      "list": [
-        "GitHub",
-        "Bitbucket",
-        "CLI",
-        "NPM",
-        "VSCode",
-        "Prettier",
-        "ESLint",
-        "Figma",
-        "Photoshop",
-        "Illustrator",
-        "Sketch"
-      ]
-    },
-    {
-      "heading": "Education",
-      "list": [
-        "Self Taught Developer",
-        "Interactive Design (Capilano University)",
-        "Apple Technical Coordinator",
-        "Apple Help Desk Specialist",
-        "Apple Server Support Specialist"
-      ]
-    },
-    {
-      "heading": "Interests",
-      "list": [
-        "Photography",
-        "Travel",
-        "Music",
-        "Food/Cooking",
-        "Snowboarding",
-        "Scuba Diving",
-        "Bush Regeneration"
-      ]
-    }
-  ]
-  
+  {
+    heading: "Languages & Libraries",
+    list: [
+      "JavaScript",
+      "Typescript",
+      "React",
+      "Vue",
+      "HTML",
+      "CSS",
+      "Git",
+      "Vite",
+      "Rollup",
+      "Webpack",
+      "GSAP",
+      "SVG",
+      "WebSockets",
+      "Express",
+      "AdonisJS",
+      "Ionic",
+      "Capacitor",
+      "Firebase",
+      "Tailwind",
+      "Twilio APIs",
+      "Pusher",
+      "TipTap/ProseMirror",
+    ],
+  },
+  {
+    heading: "Knowledge",
+    list: [
+      "SPA Architecture Design & Developement",
+      "CI/CD Methodology",
+      "Unit Testing",
+      "Integraton/e2e Testing",
+      "Web Animation",
+      "Responsive Design & Development",
+      "Front End Web Architecture",
+      "Web Accessibility",
+      "Web Perfomance Optimization",
+      "Code Optimization",
+      "Gitflow Methodology",
+      "UI/UX Design",
+      "Wireframes, User Flows, Flowcharts",
+    ],
+  },
+  {
+    heading: "Tools",
+    list: [
+      "CLI",
+      "NPM",
+      "GitHub",
+      "Bitbucket",
+      "Gitlab",
+      "Netlify",
+      "VSCode",
+      "Prettier",
+      "ESLint",
+      "Husky",
+      "Figma",
+      "Photoshop",
+      "Illustrator",
+    ],
+  },
+  {
+    heading: "Education",
+    list: [
+      "Self Taught Developer",
+      "Interactive Design (Capilano University)",
+      "Apple Technical Coordinator",
+      "Apple Help Desk Specialist",
+      "Apple Server Support Specialist",
+    ],
+  },
+  {
+    heading: "Interests",
+    list: [
+      "Photography",
+      "Travel",
+      "Music",
+      "Food/Cooking",
+      "Snowboarding",
+      "Scuba Diving",
+      "Bush Regeneration",
+    ],
+  },
+];
