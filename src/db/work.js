f0aa7eb4a8a95573404f04d05ba766b1cc48a97b
James <james.t@getreworked.com>
2022-05-19 11:32:47 +1000

    refactor: Changes work project positions

--- src/db/work.js
+++ src/db/work.js
@@ -1,24 +1,7 @@
 export const work = [
   {
-    title: "Where",
-    subTitle:
-      "TypeScript, React, React Query, Chart.js, Tailwind, Headless UI, Vite, HTML, Gitlab",
-    url: "https://www.where.team",
-    text: "<p>The team at Complish (see below) had another clever idea and wanted me to spearhead the web app's frontend architecture. There was only one requirement: we needed to produce an MVP in just a couple of months. So, we put our heads together and came up with a list of must have features for MVP and got to work.</p><p>Because the apps state would essentially be a mirror of the backend, I decided that React with React Query would be the perfect tool for the job. This was important to reach our MVP goal as React Query removed a lot of boilerplate and wiring up API responses to a state management system. Coupled with Tailwind and Headless UI, I was also able to quickly produce layouts and components to power our app. On top of that, I set out a personal goal to ensure our app was fully accessible and navigatable entirely via the keyboard.</p>",
-    images: [
-      "/images/work/where-0.png",
-      "/images/work/where-1.png",
-      "/images/work/where-2.png",
-      "/images/work/where-3.png",
-      "/images/work/where-4.png",
-      "/images/work/where-5.png",
-      "/images/work/where-6.png",
-      "/images/work/where-7.png",
-    ],
-  },
-  {
     title: "Complish",
-    subTitle: "JavaScript, Vue, Pusher, TipTap/ProseMirror, HTML, CSS, Gitlab",
+    subTitle: "JavaScript, Vue, Pusher, TipTap/ProseMirror, Gitlab",
     url: "https://team.complish.app",
     text: "<p>Joining the skilled & agile fully remote team at Complish I was excited to leap back into the startup world. My primary task: to build a collaborative asynchronous Discussions feature (think of a mash up between Google Docs & Notion). Easy, right? Well, not exactly.</p><p>Our first attempt was to use Quill along with Pusher and Vuex ORM to achieve our goal. This went pretty well, the Pusher & ORM side of things went off without a hitch however we found that Quill wasn't flexible enough for our needs. So, we started to look elsewhere and found TipTap (powered by ProseMirror). TipTap provided a nice layer for interacting with Vue components in the editor, while ProseMirror allowed us to really harness the power of the editor and customize it however we see fit. It was exactly what we needed as we were able to achieve our goal of a performant, collaborative, feature rich, and asynchronous editor.</p>",
     images: [
@@ -34,9 +17,25 @@ export const work = [
     ],
   },
   {
-    title: "Communo",
+    title: "Where",
     subTitle:
-      "TypeScript, React, Ionic, Capacitor, Tailwind, Twilio, HTML, CSS, Github",
+      "TypeScript, React, React Query, Chart.js, Tailwind, Headless UI, Vite, Gitlab",
+    url: "https://www.where.team",
+    text: "<p>The team at Complish had another clever idea and wanted me to spearhead the web app's frontend architecture. There was only one requirement: we needed to produce an MVP in just a couple of months. So, we put our heads together and came up with a list of must have features for MVP and got to work.</p><p>Because the apps state would essentially be a mirror of the backend, I decided that React with React Query would be the perfect tool for the job. This was important to reach our MVP goal as React Query removed a lot of boilerplate and wiring up API responses to a state management system. Coupled with Tailwind and Headless UI, I was also able to quickly produce layouts and components to power our app. On top of that, I set out a personal goal to ensure our app was fully accessible and navigatable entirely via the keyboard.</p>",
+    images: [
+      "/images/work/where-0.png",
+      "/images/work/where-1.png",
+      "/images/work/where-2.png",
+      "/images/work/where-3.png",
+      "/images/work/where-4.png",
+      "/images/work/where-5.png",
+      "/images/work/where-6.png",
+      "/images/work/where-7.png",
+    ],
+  },
+  {
+    title: "Communo",
+    subTitle: "TypeScript, React, Ionic, Capacitor, Tailwind, Twilio, Github",
     url: "https://www.communo.com",
     text: "<p>I had the great opportunity to work with the talented product team at Communo to help them develop their web SPA and native mobile app using Ionic React with Capacitor. My primary task was to build out the applications chat features which integrated with Twilio's programmable chat API.</p><p>The chat feature was developed in isolation from the rest of the app, including its own state management. This decoupling of the feature made it agile and easy to maintain. Components were developed with flexibility in mind so that they could be dropped into any context with minimial effort. For example, a chat conversation could be dropped in to any page for an instant contextual chat experience.</p>",
     images: [
@@ -50,7 +49,7 @@ export const work = [
   {
     title: "Casino.eu",
     subTitle:
-      "JavaScript, Vue, Vuex, Laravel, Webpack, Pusher, GSAP, HTML, SASS, SVG, BitBucket",
+      "JavaScript, Vue, Vuex, Laravel, Webpack, Pusher, GSAP, SVG, BitBucket",
     url: "https://www.casino.eu",
     text: "<p>Over the past few years, Casino.eu has grown from a rather simple hub for gamers to a full fledged responsive and immersive single-page application powered by a robust custom CMS. Users of the site enjoy their time by discovering new games with fresh offers. And, by using Pusher, we keep them up to date in real time.</p><p>Using modern development techniques such as Webpack's code splitting coupled with ES6 asynchronous imports and lazy loading images assisted by IntersectionObserver, I was able to provide an optimized and efficient experience for both mobile and desktop users. The project itself consists of over 150 different components, directives, and custom Vue plugins.</p>",
     images: [
@@ -67,7 +66,7 @@ export const work = [
   {
     title: "Casino.eu - Portal",
     subTitle:
-      "JavaScript, Vue, Vuex, Laravel, Webpack, Pusher, GSAP, HTML, SASS, SVG, BitBucket",
+      "JavaScript, Vue, Vuex, Laravel, Webpack, Pusher, GSAP, SVG, BitBucket",
     url: "https://portal.casino.eu",
     text: "<p>The backbone for Casino.eu, Portal drives content and provides the API required to power the Casino.eu ecosystem. Because of the clients unique requests, we felt it best to develop a custom CMS powered by Vue to help them manage things.</p><p>Because of Vue's simple yet powerful reactive feature set it was the ideal candidate to help us build a responsive SPA for the CMS. To assist Vue, Laravel is used to do the heavy lifting on the backend and Pusher is integrated to provide users with real time updates. As a final touch, GSAP helps bring life to a unique on boarding experience when users sign up to Casino.eu.</p>",
     images: [
@@ -81,7 +80,7 @@ export const work = [
   },
   {
     title: "Remax - Perfect Match",
-    subTitle: "JavaScript, Vue, AdonisJS, MySQL, OAuth, HTML, SASS, BitBucket",
+    subTitle: "JavaScript, Vue, AdonisJS, MySQL, OAuth, BitBucket",
     url: "https://perfectmatch.remax-malta.com/",
     text: "<p>BRND WGN approached me to take on an interesting microsite concept; a Tinder-eqsue property search tool. The concept was quite simple, a property search wizard followed by a Tinder style like or dislike feature for sorting.<p><p>The frontend, being powered by Vue, nicely integrates with a headless backend API running on Node and powered by AdonisJS coupled with MySQL. Integration with Facebook and Google (as well as email) allows users to create an account to save their liked/disliked properties.</p>",
     images: [
@@ -94,7 +93,7 @@ export const work = [
   {
     title: "GO Fibre Experience",
     subTitle:
-      "JavaScript, Vue, Phillips Hue Node API, Socket.io, Express, Webpack, HTML, SASS, BitBucket",
+      "JavaScript, Vue, Phillips Hue Node API, Socket.io, Express, Webpack, BitBucket",
     url: "",
     text: "<p>GO (one of Malta's largest telecom providers) came to BRND WGN for help in marketing their fibre network. This became a truly unique project using web technologies to create an immersive experience. Teaming up with the creatives at BRND WGN, I developed a two part web application. The first part was a dashboard interface displayed on a tablet where users were given instructions to interact with. Depending on the stage of the experience video would be either played on the dashboard or on a large mounted flatscreen.</p><p>During the experience coloured lighting would change to go with the videos. This was all powered by a Node.js Express server, WebSockets, and integration with the Phillips HUE api. A custom cueing system was developed to manage the video, audio, and lighting to ensure everything synced up perfectly.</p>",
     images: [
@@ -106,8 +105,7 @@ export const work = [
   },
   {
     title: "GO Directory App",
-    subTitle:
-      "JavaScript, Ionic, Angular 1, Cordova, Gulp, HTML, SASS, BitBucket",
+    subTitle: "JavaScript, Ionic, Angular 1, Cordova, Gulp, BitBucket",
     url: "",
     text: "<p>Whilst working at BRND WGN, GO (one of Malta's largest telecom providers) approached us to develop a directory app that allowed users to search for any publicly accessible number in Malta. GO needed this mobile app to work on both phones and tablets as well as both iOS and Android. Naturally we thought this would be a perfect use case for a hybrid app.</p><p>So, with those requirements in mind, I reached to Ionic to help get the job done. Using Angular and integrating a few extra Cordova plugins to access features such as address books, connectivity, and system dialogs, I was able to efficiently develop and turn around a production ready app within a few short weeks.</p>",
     images: [
@@ -118,7 +116,7 @@ export const work = [
   },
   {
     title: "nTrust",
-    subTitle: "JavaScript, jQuery, WebSockets, HTML, SASS, SVN",
+    subTitle: "JavaScript, jQuery, WebSockets, SVN",
     url: "",
     text: "<p>I was brought on to nTrust to help take their existing web app to the next level. Originally built solely with jQuery, I guided the front end team towards an improved JavaScript architecture using module patterns that allowed for a robust, reliable, and maintainable code base which consequently improved developer experience.</p><p>nTrust's applications were comprised of two main facets; a customer web app and a merchant web app. With each app our goal was to make transactional tasks such as remittance, deposits, and merchant payments as quick and painless as possible. Using WebSockets we were able to provide a seamless and instantaneous user experience for both customers and merchants.</p>",
     images: [
